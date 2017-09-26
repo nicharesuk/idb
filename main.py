a4069623440770971ff6d493cbc5688656b4130b
Corbin Rogerson <nicharesuk@gmail.com>
2017-09-26 03:20:04 -0500

    Fix pathing to build dir in main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='../build')
+BUILD_DIR = '/build'
+app = Flask(__name__, static_folder='./build')
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
     if(path == ""):
-        return send_from_directory('../build', 'index.html')
+        return send_from_directory('build', 'index.html')
     else:
-        if(os.path.exists("../build/" + path)):
-            return send_from_directory('../build', path)
+        if(os.path.exists("build/" + path)):
+            return send_from_directory('build', path)
         else:
-            return send_from_directory('../build', 'index.html')
+            return send_from_directory('build', 'index.html')
 
 
 if __name__ == '__main__':
