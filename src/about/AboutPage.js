9bb6470116eae3cf59a768c591709beb9f7e210d
Randy Thai <randythai1996@gmail.com>
2017-09-26 19:46:23 -0500

    Fill some more of the about page

--- src/about/AboutPage.js
+++ src/about/AboutPage.js
@@ -5,84 +5,124 @@ class AboutPage extends Component {
   render() {
     return (
 			<div className="about_container">
-				<p className="topic">
-					Meet the Anime All Stars!
-				</p>
-				<div className="ui three column stackable grid container">
- 	      	<div className="column" id="christopher">
-						<img className="img-member" src={require("./christopher.png")} alt="Christopher Gutierrez"></img>
-						<br/>
-						<p className="member_name">Christopher Gutierrez</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
-					</div>
-					<div className="column" id="corbin">
-						<img className="img-member" src={require("./corbin.jpg")} alt="Corbin Rogerson"></img>
-						<br/>
-						<p className="member_name">Corbin Rogerson</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
-					</div>
-					<div className="column" id="david">
-						<img className="img-member" src={require("./david.png")} alt="David Malone"></img>
-						<br/>
-						<p className="member_name">David Malone</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
-					</div>
-					<div className="column" id="joshua">
-						<img className="img-member" src={require("./joshua.jpg")} alt="Joshua Ondrusek"></img>
-						<br/>
-						<p className="member_name">Joshua Ondrusek</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
-					</div>
-					<div className="column" id="nelson">
-						<img className="img-member" src={require("./nelson.png")} alt="Nelson Swindler"></img>
-						<br/>
-						<p className="member_name">Nelson Swindler</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
-					</div>
-					<div className="column" id="randy">
-						<img className="img-member" src={require("./randy.jpg")} alt="Randy Thai"></img>
-						<br/>
-						<p className="member_name">Randy Thai</p>
-						<p className="member_info">About</p>
-						<p className="member_info">Role</p>
+				<div id="team">
+					<p className="topic">
+						Meet the Anime All Stars!
+					</p>
+					<div className="ui three column stackable grid container">
+						<div className="column" id="christopher">
+							<img className="img-member" src={require("./christopher.png")} alt="Christopher Gutierrez"></img>
+							<p className="member_name">Christopher Gutierrez</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Front-End Developer</p>
+						</div>
+						<div className="column" id="corbin">
+							<img className="img-member" src={require("./corbin.jpg")} alt="Corbin Rogerson"></img>
+							<p className="member_name">Corbin Rogerson</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Back-End Engineer</p>
+						</div>
+						<div className="column" id="david">
+							<img className="img-member" src={require("./david.png")} alt="David Malone"></img>
+							<p className="member_name">David Malone</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Front-End Developer</p>
+						</div>
+						<div className="column" id="joshua">
+							<img className="img-member" src={require("./joshua.jpg")} alt="Joshua Ondrusek"></img>
+							<p className="member_name">Joshua Ondrusek</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Back-End Engineer</p>
+						</div>
+						<div className="column" id="nelson">
+							<img className="img-member" src={require("./nelson.png")} alt="Nelson Swindler"></img>
+							<p className="member_name">Nelson Swindler</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Front-End Developer</p>
+						</div>
+						<div className="column" id="randy">
+							<img className="img-member" src={require("./randy.jpg")} alt="Randy Thai"></img>
+							<p className="member_name">Randy Thai</p>
+							<p className="member_info">About</p>
+							<p className="member_info">Front-End Developer</p>
+						</div>
 					</div>
 				</div>
-				<p className="topic">
-					Tools
-				</p>
-				<br/>
-				<div className="ui four column stackable grid container">
-					<div className="column" id="github">
-						<a href="https://github.com/nicharesuk/idb"><img className="img-tool" src={require("./github.png")} alt="Github"></img></a>
+				<div id="stats">
+					<p className="topic">
+						The Stats
+					</p>
+					<div className="stats_info">
+						<p>Apiary API</p>
+						<p><a href="https://github.com/nicharesuk/idb">Github Repository</a></p>
+						<p><a href="https://trello.com/b/KrhsKbmQ/weebmd">Trello Board</a></p>
 					</div>
-					<div className="column" id="googlecloudplatform">
-						<img className="img-tool" src={require("./googlecloudplatform.png")} alt="Google Cloud Platform"></img>
+				</div>
+				<div id="data">
+					<p className="topic">
+						The Data
+					</p>
+					<div className="ui two column stackable grid container" id="data">
+						<div className="column" id="anilist">
+							<p className="data_name">AniList API</p>
+							<p className="data_info"><a href="http://anilist-api.readthedocs.io/en/latest/">http://anilist-api.readthedocs.io/en/latest/</a></p>
+						</div>
+						<div className="column" id="jikan">
+							<p className="data_name">Jikan.me API</p>
+							<p className="data_info"><a href="https://jikan.me/">https://jikan.me/</a></p>
+						</div>
 					</div>
-					<div className="column" id="namecheap">
-						<img className="img-tool" src={require("./namecheap.png")} alt="Namecheap"></img>
-					</div>
-					<div className="column" id="planitpoker">
-						<img className="img-tool" src={require("./planitpoker.png")} alt="PlanITPoker"></img>
-					</div>
-					<div className="column" id="python">
-						<img className="img-tool" src={require("./python.png")} alt="PlanITPoker"></img>
-					</div>
-					<div className="column" id="reactjs">
-						<img className="img-tool" src={require("./reactjs.png")} alt="ReactJS"></img>
-					</div>
-					<div className="column" id="semanticui">
-						<img className="img-tool" src={require("./semanticui.png")} alt="SemanticUI"></img>
-					</div>
-					<div className="column" id="slack">
-						<img className="img-tool" src={require("./slack.png")} alt="Slack"></img>
-					</div>
-					<div className="column" id="trello">
-						<a href="https://trello.com/b/KrhsKbmQ/weebmd"><img className="img-tool" src={require("./trello.png")} alt="Slack"></img></a>
+				</div>
+				<div id="tools">
+					<p className="topic">
+						The Toolbox
+					</p>
+					<div className="ui four column stackable grid container">
+						<div className="column" id="apiary">
+							<img className="img-tool" src={require("./apiary.png")} alt="Apiary"></img>
+							<p className="tool_name">Apiary</p>
+							<p className="tool_info">API Design, Development & Documentation</p>
+						</div>
+						<div className="column" id="github">
+							<img className="img-tool" src={require("./github.png")} alt="GitHub"></img>
+							<p className="tool_name">GitHub</p>
+							<p className="tool_info">Version Control Repository</p>
+						</div>
+						<div className="column" id="googlecloudplatform">
+							<img className="img-tool" src={require("./googlecloudplatform.png")} alt="Google Cloud Platform"></img>
+							<p className="tool_name">Google Cloud Platform</p>
+							<p className="tool_info">Cloud Computing Service</p>
+						</div>
+						<div className="column" id="namecheap">
+							<img className="img-tool" src={require("./namecheap.png")} alt="Namecheap"></img>
+							<p className="tool_name">Namecheap</p>
+							<p className="tool_info">Domain Name Registrar</p>
+						</div>
+						<div className="column" id="planitpoker">
+							<img className="img-tool" src={require("./planitpoker.png")} alt="PlanITPoker"></img>
+							<p className="tool_name">PlanITPoker</p>
+							<p className="tool_info">Online Scrum Planning</p>
+						</div>
+						<div className="column" id="reactjs">
+							<img className="img-tool" src={require("./reactjs.png")} alt="ReactJS"></img>
+							<p className="tool_name">ReactJS</p>
+							<p className="tool_info">Javascript Library for Building UI</p>
+						</div>
+						<div className="column" id="semanticui">
+							<img className="img-tool" src={require("./semanticui.png")} alt="Semantic UI"></img>
+							<p className="tool_name">Semantic UI</p>
+							<p className="tool_info">UI Component Framework</p>
+							</div>
+						<div className="column" id="slack">
+							<img className="img-tool" src={require("./slack.png")} alt="Slack"></img>
+							<p className="tool_name">Slack</p>
+							<p className="tool_info">Team Collaboration Tools and Services</p>
+						</div>
+						<div className="column" id="trello">
+							<img className="img-tool" src={require("./trello.png")} alt="Trello"></img>
+							<p className="tool_name">Trello</p>
+							<p className="tool_info">Project Issue Tracker</p>
+						</div>
 					</div>
 				</div>
 			</div>
