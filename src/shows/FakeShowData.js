e64abf251ef4d897055d88128e0f6c7833c86e58
Nelson <nelsonswindler@utmail.utexas.edu>
2017-09-26 22:53:15 -0500

    Cleaned up styling. Added more to the fake dummy data

--- src/shows/FakeShowData.js
+++ src/shows/FakeShowData.js
@@ -6,18 +6,18 @@ export default [
     year: 2017,
   },
   {
-    title: "Death Note",
-    imageURL: "https://myanimelist.cdn-dena.com/images/anime/9/9453.jpg",
-    year: 2017,
-  },
-  {
-    title: "Steins Gate",
-    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/73199.jpg",
+    title: "One Punch Man",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
     year: 2017,
   },
   {
     title: "Tokyo Ghoul",
     imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/73199.jpg",
     year: 2017,
   },
   {
@@ -29,5 +29,105 @@ export default [
     title: "Log Horizon",
     imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/84004.jpg",
     year: 2017,
-  }
+  },
+  {
+    title: "Steins Gate 0",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/4/77633.jpg",
+    year: 2017,
+  },
+  {
+    title: "One Punch Man",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
+    year: 2017,
+  },
+  {
+    title: "Tokyo Ghoul",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/73199.jpg",
+    year: 2017,
+  },
+  {
+    title: "Sword Art Online",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/11/39717.jpg",
+    year: 2017,
+  },
+  {
+    title: "Log Horizon",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/84004.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate 0",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/4/77633.jpg",
+    year: 2017,
+  },
+  {
+    title: "One Punch Man",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
+    year: 2017,
+  },
+  {
+    title: "Tokyo Ghoul",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/73199.jpg",
+    year: 2017,
+  },
+  {
+    title: "Sword Art Online",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/11/39717.jpg",
+    year: 2017,
+  },
+  {
+    title: "Log Horizon",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/84004.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate 0",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/4/77633.jpg",
+    year: 2017,
+  },
+  {
+    title: "One Punch Man",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
+    year: 2017,
+  },
+  {
+    title: "Tokyo Ghoul",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/73199.jpg",
+    year: 2017,
+  },
+  {
+    title: "Sword Art Online",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/11/39717.jpg",
+    year: 2017,
+  },
+  {
+    title: "Log Horizon",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/5/84004.jpg",
+    year: 2017,
+  },
+  {
+    title: "Steins Gate 0",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/4/77633.jpg",
+    year: 2017,
+  },
+  {
+    title: "One Punch Man",
+    imageURL: "https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
+    year: 2017,
+  },
 ];
