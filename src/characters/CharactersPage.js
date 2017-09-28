d567220db117527b28d1bfd998796939b2275732
Nelson <nelsonswindler@utmail.utexas.edu>
2017-09-27 19:02:54 -0500

    Added characters and people data and hooked up their pages

--- src/characters/CharactersPage.js
+++ src/characters/CharactersPage.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './CharactersPage.css';
+import data from './FakeCharactersData';
+import ThumbnailPage from '../shared/ThumbnailPage';
 
 class CharactersPage extends Component {
   render() {
     return (
-      <div>
-        The characters page
-      </div>
+      <ThumbnailPage
+        isManga={false}
+        data={data.map(character => {
+          return {
+            ...character,
+            title: character.name,
+            started: character.nameJapanese,
+          }
+        })} />
     );
   }
 }
