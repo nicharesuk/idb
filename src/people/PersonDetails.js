8e2a14d1d42e8657e4856b33790a0fac3618df9e
Nelson <nelsonswindler@utmail.utexas.edu>
2017-09-27 21:27:27 -0500

    Added links for shows and manga in the people description modal

--- src/people/PersonDetails.js
+++ src/people/PersonDetails.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 // import './PersonDetails.css';
 import PropTypes from 'prop-types';
+import showData from '../shows/FakeShowData';
+import mangaData from '../manga/FakeMangaData';
 
 class PersonDetails extends Component {
+
+  getShow = () => {
+    this.props.dataObject.voiceActionRole.forEach(role => {
+      showData.forEach(show => {
+        if (role.anime.name === show.title) {
+          this.props.onChangeContent("shows", show);
+        }
+      });
+    });
+  }
+
+  getManga = () => {
+    this.props.dataObject.voiceActionRole.forEach(role => {
+      mangaData.forEach(manga => {
+        if (role.anime.name === manga.title) {
+          this.props.onChangeContent("manga", manga);
+        }
+      });
+    });
+  }
+
   render() {
     const { name, birthday, voiceActionRole } = this.props.dataObject;
     return (
       <div>
+        <a
+          style={{cursor: "pointer"}}
+          onClick={this.getShow}>
+          Link to show
+        </a><br />
+        <a
+          style={{cursor: "pointer"}}
+          onClick={this.getManga}>
+          Link to manga
+        </a>
         <p>
           {`Name: ${name}`}
         </p>
