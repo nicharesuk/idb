c1645f2f6abd9cb4abb2c013c08817844da07c78
dmalone7 <maloned101@utexas.edu>
2017-09-27 19:45:36 -0500

    added basic dimmer for mouse hovering over thumbnails

--- src/shared/ThumbnailCard.js
+++ src/shared/ThumbnailCard.js
@@ -4,15 +4,25 @@ import { Image } from 'semantic-ui-react'
 import PropTypes from 'prop-types';
 
 class ThumbnailCard extends Component {
+  state = {}
+
+  handleShow = () => this.setState({ active: true })
+  handleHide = () => this.setState({ active: false })
 
   render() {
+    const { active } = this.state
     const unitName = this.props.isManga ? "chapters" : "chapters"
     return (
-      <div className="thumbnailCardContainer">
-        <Image
-          width="134px"
-          height="196px"
-          src={this.props.imageURL} />
+      <div className="thumbnailCardContainer" onMouseEnter={this.handleShow} onMouseLeave={this.handleHide}>
+        <Dimmer.Dimmable dimmed={active}>
+            <Dimmer active={active} >
+               {this.props.title}
+            </Dimmer>
+          <Image
+            width="134px"
+            height="196px"
+            src={this.props.imageURL} />
+        </Dimmer.Dimmable>
         <div className="thumbnailTitle">
           <div>
             {this.props.title}
