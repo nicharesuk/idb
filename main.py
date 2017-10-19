28fb4a3caa48b997334266de8f0c05e41fddcb32
Corbin Rogerson <nicharesuk@gmail.com>
2017-10-18 21:27:57 -0500

    Setting up testing environment files, adding infrastructure
    Fixing 404 on default path
    
    
    Remove make file in favor of npm scripts
    
    
    Merge remote-tracking branch 'origin/test_proxy' into test_proxy
    
    
    Revert "Merge remote-tracking branch 'origin/test_proxy' into test_proxy"
    
    This reverts commit 73badb4c2666408c1adc1b634055ed1a6a170b0f.

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import os
 from flask import Flask, send_from_directory
+import app.models
 
-BUILD_DIR = '/build'
-app = Flask(__name__, static_folder='./build')
+BUILD_DIR = 'build'
+flask_server = Flask(__name__, static_folder=BUILD_DIR)
 
-# Serve React App
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+# Serve React App, handle all unhandled paths
+@flask_server.route('/', defaults={'path': ''})
+@flask_server.route('/<path:path>')
 def serve(path):
-    if(path == ""):
-        return send_from_directory('build', 'index.html')
+    if path == "":
+        return send_from_directory(BUILD_DIR, 'index.html')
     else:
-        if(os.path.exists("build/" + path)):
-            return send_from_directory('build', path)
+        if os.path.exists(BUILD_DIR + '/' + path):
+            return send_from_directory(BUILD_DIR, path)
         else:
-            return send_from_directory('build', 'index.html')
+            return send_from_directory(BUILD_DIR, 'index.html')
 
+
+@flask_server.route('/api/anime/<int:id>')
+def get_anime(id):
+    return app.models.anime(id)
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
+    flask_server.run(use_reloader=True, port=5000, threaded=True)
