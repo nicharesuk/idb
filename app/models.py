b3ca38e69853f730f0d99dc6521b44c85bc8df45
Corbin Rogerson <nicharesuk@gmail.com>
2017-10-21 19:00:15 -0500

    Potentially working models

--- app/models.py
+++ app/models.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import CheckConstraint
 
+# app = Flask(__name__)
+
+# # CORS(app, headers=['Content-Type'])
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weeb.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Suppress warning
+
 db = SQLAlchemy()
 
-actor_anime = db.Table('actor_anime',
-    db.Column('actor_id', db.Integer, db.ForeignKey('actor.id'), primary_key=True),
-    db.Column('anime_id', db.Integer, db.ForeignKey('anime.id'), primary_key=True)
+anime_character = db.Table('anime_character',
+    db.Column('anime_id', db.Integer, db.ForeignKey('anime.id'), primary_key=True),
+    db.Column('character_id', db.Integer, db.ForeignKey('character.id'), primary_key=True)
 )
 
 actor_character = db.Table('actor_character',
@@ -15,8 +21,6 @@ actor_character = db.Table('actor_character',
 )
 
 class Anime(db.Model):
-    __tablename__ = 'anime'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
@@ -29,35 +33,32 @@ class Anime(db.Model):
     status = db.Column(db.String(80), nullable=False)
 
     manga = db.relationship('Manga', backref='anime', uselist=False, lazy=True)
-    characters = db.relationship('Character', backref='anime', lazy=True)
+    anime_characters = db.relationship('Anime', secondary=anime_character, backref=db.backref('animes', lazy=True))
 
     def __repr__(self):
         return '<Anime %r>' % self.title
 
 class Manga(db.Model):
-    __tablename__ = 'manga'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True, nullable=False)
     author = db.Column(db.String(80), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     genre = db.Column(db.String(80), nullable=False)
     score = db.Column(db.Integer, nullable=False)
-    num_episodes = db.Column(db.Integer, nullable=False)
+    num_chapters = db.Column(db.Integer, nullable=False)
     synopsis = db.Column(db.Text, nullable=False)
     showType = db.Column(db.String(80), nullable=False)
     picture = db.Column(db.Text, nullable=False)
     status = db.Column(db.String(80), nullable=False)
 
     anime_id = db.Column(db.Integer, db.ForeignKey('anime.id'))
-    characters = db.relationship('Character', backref='manga', lazy=True)
+    manga_characters = db.relationship('Character', backref=db.backref('manga', lazy=True))
 
     def __repr__(self):
         return '<Manga %r>' % self.title
 
 class Character(db.Model):
-    __tablename__ = 'character'
-    __table_args__ = (CheckConstraint('NOT(anime_id IS NULL AND manga_id IS NULL)'),)
+    # __table_args__ = (CheckConstraint('NOT(anime_id IS NULL AND manga_id IS NULL)'),)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
@@ -69,15 +70,14 @@ class Character(db.Model):
 
     anime_id = db.Column(db.Integer, db.ForeignKey('anime.id'))
     manga_id = db.Column(db.Integer, db.ForeignKey('manga.id'))
-
-    actors = db.relationship("Actor", secondary=actor_character, back_populates="characters")
+    
+    anime = db.relationship('Anime', secondary=anime_character, backref=db.backref('characters', lazy=True))
+    actors_of_character = db.relationship('Actor', secondary=actor_character, backref=db.backref('characters', lazy=True))
 
     def __repr__(self):
         return '<Character %r>' % self.name
 
 class Actor(db.Model):
-    __tablename__ = 'actor'
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
@@ -85,8 +85,7 @@ class Actor(db.Model):
     birthday = db.Column(db.DateTime)
     picture = db.Column(db.Text, nullable=False)
 
-    anime = db.relationship("Anime", secondary=actor_anime, back_populates="actors")
-    characters = db.relationship("Character", secondary=actor_character, back_populates="actors")
+    characters_of_actor = db.relationship('Character', secondary=actor_character, backref=db.backref('actors', lazy=True))
 
     def __repr__(self):
         return '<Actor %r>' % self.name
