556c2ea9312e361d0bd954731db5c150df2dda0d
Randy Thai <randythai1996@gmail.com>
2017-10-22 19:03:14 -0500

    merge and restructure about page into homepage

---
+++ src/homepage/ToolData.js
@@ -0,0 +1,62 @@
+export default [
+	{
+		name: "Apiary",
+		imgURL: "https://i.lensdump.com/i/TD95o.md.png",
+		info: "API Design Development & Documentation"
+	},
+	{
+		name: "Flask",
+		imgURL: "https://i.lensdump.com/i/TDYNA.png",
+		info: "Web Framework"
+	},
+	{
+		name: "Github",
+		imgURL: "https://i.lensdump.com/i/TD3gC.md.png",
+		info: "Version Control Repository"
+	},
+	{
+		name: "Google Cloud Platform",
+		imgURL: "https://i.lensdump.com/i/TD4s5.md.png",
+		info: "Cloud Computing Service"
+	},
+	{
+		name: "Namecheap",
+		imgURL: "https://i.lensdump.com/i/TD7WT.md.png",
+		info: "Domain Name Registrar"
+	},
+	{
+		name: "PlanITPoker",
+		imgURL: "https://i.lensdump.com/i/TDOVb.png",
+		info: "Online Scrum Planning"
+	},
+	{
+		name: "ReactJS",
+		imgURL: "https://i.lensdump.com/i/TDeH3.md.png",
+		info: "Javascript UI Library"
+	},
+	{
+		name: "React Router",
+		imgURL: "https://i.lensdump.com/i/TDuWM.png",
+		info: "Declarative Routing"
+	},
+	{
+		name: "SemanticUI",
+		imgURL: "https://i.lensdump.com/i/TDnX0.png",
+		info: "UI Component Framework"
+	},
+	{
+		name: "Slack",
+		imgURL: "https://i.lensdump.com/i/TDLgD.md.png",
+		info: "Team Collaboration Tools"
+	},
+	{
+		name: "SQLite",
+		imgURL: "https://i.lensdump.com/i/TDD9Q.png",
+		info: "SQL Database Engine"
+	},
+	{
+		name: "Trello",
+		imgURL: "https://i.lensdump.com/i/TDM3q.md.png",
+		info: "Issue Tracker"
+	}
+]
