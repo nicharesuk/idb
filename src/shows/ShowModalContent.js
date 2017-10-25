aa153ff75c9957f8f062cc581815dbbcab703934
Nelson <nelsonswindler@utmail.utexas.edu>
2017-10-25 18:41:18 -0500

    Fixed broken link issue

--- src/shows/ShowModalContent.js
+++ src/shows/ShowModalContent.js
@@ -27,7 +27,7 @@ class ShowModalContent extends Component {
       <ModalDetails
         title={title}
         detailsList={[aired, `${num_episodes}`, status, rating, score, genre ]}
-        website={`https://www.youtube.com/watch?v=${youtube_id}`}
+        website={youtube_id ? `https://www.youtube.com/watch?v=${youtube_id}` : null}
         websiteText={"Link to trailer"}
         paragraph={synopsis}
         primaryList={characterList}
