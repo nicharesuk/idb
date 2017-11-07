473862819d7f9b4085a6344961f1da243c203119
Corbin Rogerson <nicharesuk@gmail.com>
2017-11-06 23:45:38 -0600

    Add top anime

--- data_retrieval/fixjikanfiles.py
+++ data_retrieval/fixjikanfiles.py
@@ -3,49 +3,9 @@ import os
 
 replacements = {'&#039;':'\'', '&quot;': '\\\"', '\\&': '&'}
 
-def fix_anime():
-
-    for filename in os.listdir('jikan_anime'):
-        read_file = 'jikan_anime/' + filename
-        lines = []
-        with open(read_file) as infile:
-            for line in infile:
-                for src, target in replacements.iteritems():
-                    line = line.replace(src, target)
-                lines.append(line)
-        with open(read_file, 'w') as outfile:
-            for line in lines:
-                outfile.write(line)
-
-def fix_manga():
-    for filename in os.listdir('jikan_manga'):
-        read_file = 'jikan_manga/' + filename
-        lines = []
-        with open(read_file) as infile:
-            for line in infile:
-                for src, target in replacements.iteritems():
-                    line = line.replace(src, target)
-                lines.append(line)
-        with open(read_file, 'w') as outfile:
-            for line in lines:
-                outfile.write(line)
-
-def fix_character():
-    for filename in os.listdir('jikan_character'):
-        read_file = 'jikan_character/' + filename
-        lines = []
-        with open(read_file) as infile:
-            for line in infile:
-                for src, target in replacements.iteritems():
-                    line = line.replace(src, target)
-                lines.append(line)
-        with open(read_file, 'w') as outfile:
-            for line in lines:
-                outfile.write(line)
-
-def fix_person():
-    for filename in os.listdir('jikan_person'):
-        read_file = 'jikan_person/' + filename
+def fix_data(directory):
+    for filename in os.listdir(directory):
+        read_file = directory + '/' + filename
         lines = []
         with open(read_file) as infile:
             for line in infile:
@@ -57,7 +17,7 @@ def fix_person():
                 outfile.write(line)
 
 if __name__ == "__main__":
-    # fix_anime()
-    # fix_manga()
-    # fix_character()
-    # fix_person()
+    fix_data('jikan_anime')
+    fix_data('jikan_manga')
+    fix_data('jikan_person')
+    fix_data('jikan_character')
