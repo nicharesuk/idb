2e013db51783807fd842631756e381b88124d298
Chris <christopherjgutierrez@utexas.edu>
2017-11-09 16:53:26 -0600

    fixed tests

--- app/test.py
+++ app/test.py
@@ -11,56 +11,52 @@ class TestAPI(unittest.TestCase):
     def test_animes(self):
         file = open("Test Data/anime/animes", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/animes")
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_animes_individual_1(self):
 
         file = open("Test Data/anime/1", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/animes/1")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
+
 
     def test_animes_individual_2(self):
 
         file = open("Test Data/anime/100", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/animes/100")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_animes_individual_3(self):
 
         file = open("Test Data/anime/50", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/animes/50")		
         actual1 = r.json()
-        actual = ordered(actual1)	
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
+
+		
+
+
+
 
     def test_animes_linked_one_relation(self):
 
         file = open("Test Data/anime/25supp", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/animes/25?include=mangas")		
         actual1 = r.json()
-        actual = ordered(actual1)	
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
 
     def test_animes_linked_two_relations(self):
@@ -85,26 +81,24 @@ class TestAPI(unittest.TestCase):
         actual = ordered(actual1)
         self.assertEqual(expected, actual)
 
+
+       
+
     def test_mangas(self):
         file = open("Test Data/manga/mangas", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/mangas")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
-
+        self.assertEqual(expected1, actual1)
 
     def test_mangas_individual(self):
         file = open("Test Data/manga/1", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/mangas/1")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_mangas_linked_one_relations(self):
         file = open("Test Data/manga/22supp", "r")
@@ -129,22 +123,18 @@ class TestAPI(unittest.TestCase):
     def test_characters(self):
         file = open("Test Data/character/characters", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/characters")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_characters_individual(self):
         file = open("Test Data/character/1", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/characters/1")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_characters_linked_one_relations(self):
         file = open("Test Data/character/50supp", "r")
@@ -169,12 +159,10 @@ class TestAPI(unittest.TestCase):
     def test_actors_individual(self):
         file = open("Test Data/actor/1", "r")
         expected1 = json.load(file)
-        expected = ordered(expected1)
         file.close()
         r = s.get("http://www.weebmd.me/api/actors/1")		
         actual1 = r.json()
-        actual = ordered(actual1)
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected1, actual1)
 
     def test_actors_linked_one_relations(self):
         file = open("Test Data/actor/50supp", "r")
@@ -195,8 +183,6 @@ class TestAPI(unittest.TestCase):
         actual1 = r.json()
         actual = ordered(actual1)
         self.assertEqual(expected, actual)
-
-
 
 
 
