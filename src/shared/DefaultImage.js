25d2c34c8901c7c8bebeba428023a47d096070e7
Nelson <nelsonswindler@utmail.utexas.edu>
2017-11-09 13:08:53 -0600

    Changed hosting to imgur

--- src/shared/DefaultImage.js
+++ src/shared/DefaultImage.js
@@ -1,2 +1,2 @@
 
-export default "https://lh3.googleusercontent.com/An3-0UNlKyZR92GHbbHJECT_PjWkR-mV3J5b5lwjLznYxrKXYjHlLrliS2KyRiqOSa-q0Wc_QBeuWE6TL0-j=w1440-h826";
+export default "https://i.imgur.com/bjbO8oH.jpg";
