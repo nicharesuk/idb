da1c170425a9f42ad54903e634e47a52576e2c3d
Corbin Rogerson <nicharesuk@gmail.com>
2017-11-29 22:44:24 -0600

    Adds heatmap visualization

--- Visualization/getdata.py
+++ Visualization/getdata.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+api_key = "[PLACE API KEY HERE]"
+
 
 def getd3data():
     global s
@@ -47,7 +49,44 @@ def getd3data():
                     r2 = r2.json()
                     outfile.write("\n" + r2["city"]["name"].encode("utf-8") + "," + str(len(r2["artists"])))
 
+def getHeatMapInfo():
+    url = "http://poupon.me/api/cities"
+    r = requests.get(url)
+    if r.status_code == 200:
+        r = r.json()
+
+        city_location = {}
+        for city in r:
+            city_state_str = city["name"] + ", " + city["state"]
+            geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city_state_str.replace(" ", "+") + api_key
+            print(geocode_url)
+
+            num_artists = 0
+            r2 = requests.get(url + "/" + str(city["city_id"]))
+            if r2.status_code == 200 :
+                r2 = r2.json()
+                num_artists = len(r2["artists"])
+
+            g = requests.get(geocode_url)
+            if g.status_code == 200:
+                g = g.json()
+                
+                lat_lng = g["results"][0]["geometry"]["location"]
+                lat = lat_lng["lat"]
+                lng = lat_lng["lng"]
+
+                print(lat_lng)
+
+                city_location[city["city_id"]] = (str(num_artists), city["name"], str(lat), str(lng))
+
+        print(city_location)
+
+        with open("cities-lived.csv", 'w') as outfile:
+            outfile.write("years,place,lat,lon")
+            for key, value in city_location.items():
+                outfile.write("\n" + ",".join(value))
 
 
 if __name__ == "__main__" :
+
     getd3data()
